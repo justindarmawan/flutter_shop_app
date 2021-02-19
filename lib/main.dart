fa944561a9447ae58d4a16e3d64f27a0ec666ce8
Justin <jdarmawan.jd@gmail.com>
2021-02-19 15:00:50 +0700

    Setup Folder and Model

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shop'),
+      ),
+      body: Center(
+        child: Text('build a shop'),
+      ),
+    );
+  }
+}
