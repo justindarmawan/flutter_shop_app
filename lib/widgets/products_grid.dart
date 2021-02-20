887146100cc1b00df5d03d25d6e63aadf02086c0
Justin <jdarmawan.jd@gmail.com>
2021-02-21 00:04:14 +0700

    Adding Provider State to Each Grid Items

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -13,11 +13,14 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (context, index) {
-        return ProductItem(
-          products[index].id,
-          products[index].title,
-          products[index].imageUrl,
+      itemBuilder: (ctx, index) {
+        return ChangeNotifierProvider(
+          create: (c) => products[index],
+          child: ProductItem(
+              // products[index].id,
+              // products[index].title,
+              // products[index].imageUrl,
+              ),
         );
       },
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
